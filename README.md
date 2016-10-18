30e1ef3c4a57fe9dae1e8393c5bd6aef23a3e82b
Kathiravan Kumarasamy <kathiravan.kumarasamy@TheoremIndia.net>
2016-10-18 16:12:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# A Simple Responsive Shopping Cart page
+
+## To Build and Run the project
+
+In order to Check App functionality, ensure that you have **[Git](http://git-scm.com/downloads)** and **[Node.js](http://nodejs.org)** installed.
+
+Clone a copy of the repo: 
+
+```
+git clone https://github.com/kathirr007/Responsive-shopping-cart.git
+```
+
+Change to the Project directory:
+
+```
+cd Responsive-shopping-cart
+```
+
+Install build tools and dev dependencies:
+
+```
+npm install -g gulp
+npm install
+```
+
+Exectute the following to check the functionality in browser
+
+```
+
+## For Develolment version
+
+...
+gulp                 # Build development version of App in `/builds/development`
+...
+
+## For production version
+
+Change local NODE_ENV to production and execute the following
+
+...
+gulp                 # Build production version of App in `/builds/production`
+```
---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat'),
+	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
+	browserify = require('gulp-browserify'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyhhtml = require('gulp-minify-html'),
+	jsonminify = require('gulp-jsonminify'),
+	imagemin = require('gulp-imagemin'),
+	pngcrush = require('imagemin-pngcrush'),
+	clean = require('del');
+
+var env,
+	coffeeSources,
+	jsSources,
+	sassSources,
+	htmlSources,
+	jsonSources,
+	sassStyle,
+	outputDir;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env==='development') {
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+} else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = ['components/scripts/rclick.js',
+				 'components/scripts/pixgrid.js',
+				 'components/scripts/tagline.js',
+				 'components/scripts/template.js'
+];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
+
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(browserify())
+	.pipe(gulpif(env==='production', uglify()))
+	.pipe(gulp.dest(outputDir + 'js'))
+	.pipe(connect.reload())
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		css: outputDir + 'css',
+		images: outputDir + 'images',
+		style: sassStyle
+	}))
+		.on('error', gutil.log)
+	// .pipe(gulp.dest('builds/development/css'))
+	.pipe(connect.reload())
+});
+
+gulp.task('html', function(){
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env==='production', minifyhhtml()))
+	.pipe(gulpif(env==='production', gulp.dest(outputDir)))
+	.pipe(connect.reload())
+});
+gulp.task('images', function(){
+	gulp.src('builds/development/images/**/*.*')
+	.pipe(gulpif(env==='production', imagemin({
+		progressive: true,
+		svgoPlugin: [{removeViewBox: false}],
+		use: [pngcrush()]
+	})))
+	.pipe(gulpif(env==='production', gulp.dest(outputDir + 'images')))
+	.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env==='production', minifyhhtml()))
+	.pipe(gulpif(env==='production', gulp.dest(outputDir + 'js')))
+	.pipe(connect.reload())
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: outputDir,
+		livereload: true
+	});
+});
+
+gulp.task('watch', function(){
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('builds/development/js/*.json', ['json']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch('builds/development/images/**/*.*', ['images']);
+	gulp.watch('builds/development/*.html', ['html']);
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'images', 'connect', 'html', 'json', 'watch']);
